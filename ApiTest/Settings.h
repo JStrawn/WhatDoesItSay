6e9da929092b9a4ab94489b92694fe0503187797
Emi Clark <emiko.clark@gmail.com>
2017-03-31 14:44:52 -0400

    re-add settings

---
+++ ApiTest/Settings.h
@@ -0,0 +1,22 @@
+//
+//  Settings.h
+//  ApiTest
+//
+//  Created by Emiko Clark on 3/31/17.
+//  Copyright © 2017 JStrawn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Settings : NSObject 
+
+@property (strong, nonatomic) NSArray *sourceLanguagesList;
+@property (strong, nonatomic) NSArray *targetLanguagesList;
+
+@property (strong, nonatomic)  NSString *targetLanguage;
+@property (strong, nonatomic)  NSString *sourceLanguage;
+
+-(void) checkLanguageSettings;
+- (void) save;
+
+@end
